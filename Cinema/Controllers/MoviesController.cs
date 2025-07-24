475c9d6e253a2e56bf52f5ea7bfc8dc6b3d857f1
MariaGenova36 <mariq.genova@gmail.com>
2025-07-24 20:21:53 +0300

    Search,Filter,Sort

--- Cinema/Controllers/MoviesController.cs
+++ Cinema/Controllers/MoviesController.cs
@@ -20,11 +20,56 @@ namespace Cinema.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string search, int? genreId, string sortOrder)
         {
-            var applicationDbContext = _context.Movies.Include(m => m.Genre);
-            return View(await applicationDbContext.ToListAsync());
+            ViewData["CurrentFilter"] = search;
+            ViewData["CurrentGenre"] = genreId;
+            ViewData["CurrentSort"] = sortOrder;
+
+            ViewData["TitleSort"] = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewData["ReleaseSort"] = sortOrder == "release_asc" ? "release_desc" : "release_asc";
+            ViewData["DurationSort"] = sortOrder == "duration_asc" ? "duration_desc" : "duration_asc";
+
+            var movies = _context.Movies.Include(m => m.Genre).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                movies = movies.Where(m => m.Title.Contains(search) || m.Description.Contains(search));
+            }
+
+            if (genreId.HasValue)
+            {
+                movies = movies.Where(m => m.GenreId == genreId);
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    movies = movies.OrderByDescending(m => m.Title);
+                    break;
+                case "release_asc":
+                    movies = movies.OrderBy(m => m.ReleaseDate);
+                    break;
+                case "release_desc":
+                    movies = movies.OrderByDescending(m => m.ReleaseDate);
+                    break;
+                case "duration_asc":
+                    movies = movies.OrderBy(m => m.Duration);
+                    break;
+                case "duration_desc":
+                    movies = movies.OrderByDescending(m => m.Duration);
+                    break;
+                default:
+                    movies = movies.OrderBy(m => m.Title);
+                    break;
+            }
+
+            ViewBag.Genres = new SelectList(await _context.Genres.ToListAsync(), "Id", "Name");
+
+            return View(await movies.ToListAsync());
         }
+
+
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
