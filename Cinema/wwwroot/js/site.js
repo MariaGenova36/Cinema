df406b73a626ad6c0582bd032ad29fcafc0e2fe1
MariaGenova36 <mariq.genova@gmail.com>
2025-09-29 22:13:31 +0300

    Button back to top

--- Cinema/wwwroot/js/site.js
+++ Cinema/wwwroot/js/site.js
@@ -59,3 +59,20 @@
 
     updatePrice();
 });
+
+//Button back to top
+
+
+window.onscroll = function () {
+    const btn = document.getElementById("backToTopBtn");
+    if (!btn) return;
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
